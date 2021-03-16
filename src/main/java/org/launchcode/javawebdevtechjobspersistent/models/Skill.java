7207ffabb08b2c97c2f74ef1a2cd2e73c9c91879
Jeremy Gathing <bigguy@Jeremys-Mac-mini.local>
2021-03-15 19:31:56 -0500

    Second Commit, added Skill & Employer

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,23 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank
+    @Size(min = 5, max = 500, message = "Please enter description between 5 and 500 characters!!")
+    private String description;
+
+    public Skill() {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
